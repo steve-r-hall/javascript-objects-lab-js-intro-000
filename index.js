d795e59f96200a9ab0efed95f0a198910232b1c1
Stephen Robert hall <steve.r.hall@outlook.com>
2019-08-22 12:43:28 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var recipes = {};
 function updateObjectWithKeyAndValue(object, key, value) {
    var object = Object.assign({}, recipes);
   Object.assign(object, {[key]: value});
-   return object }
+   return object 
 }
 
 
