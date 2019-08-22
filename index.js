7b1bfde158f0c447f877707729067ce5524f5e3a
Stephen Robert hall <steve.r.hall@outlook.com>
2019-08-22 12:42:56 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -9,16 +9,16 @@ function updateObjectWithKeyAndValue(object, key, value) {
 
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
       Object.assign(object, {[key]: value});
-   return object 
+   return object
 }
 
 function deleteFromObjectByKey(object, key)' {
      var object = Object.assign({}, recipes);
      delete object.[key];
-   return object 
+   return object
 }
 
 function destructivelyDeleteFromObjectByKey(object, key) {
   delete object.[key];
-return object 
+return object
 }
